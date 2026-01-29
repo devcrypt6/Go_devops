359e8a347cbf5cbcfa9cb8c5af09b0da9f58360b
Sushank <sushankgoud1@gmail.com>
2026-01-29 18:21:56 +0100

    feat(main): add main function to call control flow demonstrations

---
+++ 5_control_flow/main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	// Call the IfElseDemo function from if_else.go
+	IfElseDemo()
+
+	// Call the for_1 function from for_1.go
+	for_1()
+
+	// Call the for_2 function from for_2.go
+	for_2()
+
+	// Call the for_3 function from for_3.go
+	for_3()
+}
